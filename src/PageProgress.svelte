4610d499c01136c9363c92d5501d9469c954d30b
Lucretius Biah <lucretiusbiah@protonmail.com>
2021-04-30 15:31:35 +0100

    Added a new prop
    Turn the z-index into a prop. this will allow the user to set it from the component

--- src/PageProgress.svelte
+++ src/PageProgress.svelte
@@ -5,6 +5,7 @@
   export let color = "skyblue";
   export let height = "4px";
   export let position = "top";
+  export let zIndex = 999;
   export let opacity = 1;
   const { style } = document.documentElement
 
@@ -12,6 +13,8 @@
   $: style.setProperty("--spp-height", height)
   $: style.setProperty("--spp-width", width)
   $: style.setProperty("--spp-opacity", opacity)
+  $: style.setProperty("--spp-zindex", zIndex)
+
   const watchScrolling = () => {
     const { scrollHeight, clientHeight, scrollTop } = document.documentElement
     const winScroll = document.body.scrollTop || scrollTop
@@ -38,7 +41,7 @@
     margin: 0;
     padding: 0;
     position: fixed;
-    z-index: 999999;
+    z-index: var(--spp-zindex);
     background: var(--spp-color);
     height: var(--spp-height);
     width: var(--spp-width);
